0cff255a52ab7f3e63a34987136040ee8e29a9cd
David Ryabko <davidryabko13@gmail.com>
2022-05-21 12:23:56 +0300

    Diploma-27: Implemented not provided token authentication test

--- DiplomaProject/DiplomaProject/Tests/ApiAuthenticationTest.cs
+++ DiplomaProject/DiplomaProject/Tests/ApiAuthenticationTest.cs
@@ -11,12 +11,16 @@ namespace DiplomaProject.Tests;
 public class ApiAuthenticationTest : BaseTest
 {
     private ProjectService _projectServiceUserWithInvalidToken = null!;
+    private ProjectService _projectServiceUnauthorizedUser = null!;
 
     [OneTimeSetUp]
     public void SetUpInvalidClients()
     {
         var clientWithInvalidToken = new RestClientExtended(UserType.WithInvalidAuthenticationData);
         _projectServiceUserWithInvalidToken = new ProjectService(clientWithInvalidToken);
+        
+        var unauthorizedClient = new RestClientExtended(UserType.Unauthorized);
+        _projectServiceUnauthorizedUser = new ProjectService(unauthorizedClient);
     }
 
     [Test]
@@ -27,5 +31,15 @@ public class ApiAuthenticationTest : BaseTest
 
         RestClientExtended.LastCallResponse.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
         RestClientExtended.LastCallResponse.Content.Should().Contain("API token is invalid");
+    }
+    
+    [Test]
+    [Category("Negative")]
+    public void RequestNoAuthentication()
+    {
+        _projectServiceUnauthorizedUser.GetAllProjects().Wait();
+
+        RestClientExtended.LastCallResponse.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        RestClientExtended.LastCallResponse.Content.Should().Contain("API token not provided");
     }
 }
