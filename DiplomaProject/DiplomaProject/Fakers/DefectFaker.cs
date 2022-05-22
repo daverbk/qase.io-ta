993b792c71ececf0c59be4d474a6621235099fad
David Ryabko <davidryabko13@gmail.com>
2022-05-22 11:09:27 +0300

    Diploma-12: Renamed two fields to prevent confusion

--- DiplomaProject/DiplomaProject/Fakers/DefectFaker.cs
+++ DiplomaProject/DiplomaProject/Fakers/DefectFaker.cs
@@ -5,13 +5,13 @@ namespace DiplomaProject.Fakers;
 
 public class DefectFaker : Faker<Defect>
 {
-    private const int MinSeverity = 6;
-    private const int MaxSeverity = 1;
+    private const int LowSeverity = 6;
+    private const int HighSeverity = 1;
 
     public DefectFaker()
     {
         RuleFor(project => project.Title, faker => faker.Commerce.ProductName());
-        RuleFor(project => project.Severity, faker => faker.Random.Int(MaxSeverity, MinSeverity).ToString());
+        RuleFor(project => project.Severity, faker => faker.Random.Int(HighSeverity, LowSeverity).ToString());
         RuleFor(project => project.ActualResult, faker => faker.Company.CatchPhrase());
     }
 }
