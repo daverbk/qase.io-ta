81e43d1f3a682692c77a08dabd631b0267cffe3d
David Ryabko <davidryabko13@gmail.com>
2022-05-25 21:58:05 +0300

    NO-CASE: Implemented the example of Mediator pattern

--- DiplomaProject/DiplomaProject/Steps/LoginStep.cs
+++ DiplomaProject/DiplomaProject/Steps/LoginStep.cs
@@ -1,4 +1,5 @@
 using DiplomaProject.Pages;
+using NUnit.Allure.Attributes;
 using OpenQA.Selenium;
 
 namespace DiplomaProject.Steps;
@@ -7,18 +8,22 @@ public class LoginStep
 {
     private readonly IWebDriver _webDriver;
     private readonly WelcomingPage _welcomingPage;
+    private readonly AuthorizationPage _authorizationPage;
 
     public LoginStep(IWebDriver webDriver)
     {
         _webDriver = webDriver;
         _welcomingPage = new WelcomingPage(_webDriver);
+        _authorizationPage = new AuthorizationPage(_webDriver);
     }
 
     [AllureStep("Log in with {0} and {1}")]
     public ProjectsPage LogIn(string email, string password)
     {
         _welcomingPage
-            .ProceedToLoggingIn()
+            .ProceedToLoggingIn();
+
+        _authorizationPage
             .PopulateAuthorizationData(email, password)
             .SubmitAuthorizationForm();
 
